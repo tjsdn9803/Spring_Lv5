b5da13fe227740c05f48c15eb17c3ee175530ece
tjsdn9803 <leeinjoo2@inu.ac.kr>
2023-06-26 14:22:48 +0900

    init commit

---
+++ src/main/java/com/sparta/spartalevel1/SpartaLevel1Application.java
@@ -0,0 +1,13 @@
+package com.sparta.spartalevel1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpartaLevel1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpartaLevel1Application.class, args);
+    }
+
+}
