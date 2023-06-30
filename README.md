846cdcb0633395a8fcb51540ddecd7ee7a6d2baf
tjsdn9803 <leeinjoo2@inu.ac.kr>
2023-06-30 21:08:22 +0900

    README파일 생성

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-# 항해99 15기 주특기주차 Lv.1 과제
+# 항해99 15기 주특기주차 Lv.2 과제
 
 
-- [api 설계도 노션 사이트](https://maze-mousepad-7a9.notion.site/99-15-Lv-1-9605a739f30843e58a60b00d99fe74e6?pvs=4)
+- [api 설계도 노션 사이트](https://maze-mousepad-7a9.notion.site/99-15-Lv-2-f6217a307219491398bd2e9c16dbd0cb?pvs=4)
 - ![Use Case](usecase.JPG "Use Case")
 
 ----
--- src/main/java/com/sparta/spartalevel1/dto/SignupRequestDto.java
+++ src/main/java/com/sparta/spartalevel1/dto/SignupRequestDto.java
@@ -1,18 +1,20 @@
 package com.sparta.spartalevel1.dto;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.*;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class SignupRequestDto {
-    @Pattern(regexp = "[a-z0-9]")
+    @Size(min=4, max = 10, message = "4자 이상 10자 이하여야 합니다.")
+    @Pattern(regexp = "^[a-z0-9]*$")
     private String username;
-    @Pattern(regexp = "[A-z0-9]")
+
+    @Size(min=8, max = 15, message = "8자 이상 15자 이하여야 합니다.")
+    @Pattern(regexp = "^[a-zA-Z0-9]*$")
     private String password;
 
+    @Email
     private String email;
 }
